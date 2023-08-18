ae0809349cbe4e085c492df3dc0dd2c46b80a013
Andres Riacos <popoeto@gmail.com>
2023-08-18 01:39:14 -0500

    to-do list terminado

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createTaskItem(taskText) {
   taskTextSpan.textContent = taskText; // Descripción sin enumeración
 
   const completeBtn = document.createElement("button");
-  completeBtn.textContent = "Complete";
+  completeBtn.textContent = "Realizado";
   completeBtn.classList.add("complete-btn");
 
   completeBtn.addEventListener("click", () => {
@@ -29,13 +29,13 @@ function createTaskItem(taskText) {
     if (taskItem.classList.contains("completed")) {
       completedTasksCount++;
     } else {
-      completedTasksCount--;
+      completedTasksCount--;ds
     }
     updateCounts();
   });
 
   const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
+  deleteBtn.textContent = "Eliminar";
   deleteBtn.classList.add("delete-btn");
 
   deleteBtn.addEventListener("click", () => {
@@ -69,3 +69,55 @@ document.getElementById("addTaskBtn").addEventListener("click", () => {
     taskInput.value = "";
   }
 });
+
+function createTaskItem(taskText) {
+  tasksCount++; // Incrementa el contador de tareas totales
+
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("task");
+
+  const taskDescription = document.createElement("div");
+  taskDescription.textContent = taskText;
+  taskDescription.classList.add("task-description");
+
+  const taskButtons = document.createElement("div");
+  taskButtons.classList.add("task-buttons");
+
+  const completeBtn = document.createElement("button");
+  completeBtn.textContent = "Realizado";
+  completeBtn.classList.add("complete-btn");
+
+  completeBtn.addEventListener("click", () => {
+    taskItem.classList.toggle("completed");
+    if (taskItem.classList.contains("completed")) {
+      completedTasksCount++;
+    } else {
+      completedTasksCount--;
+    }
+    updateCounts();
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Eliminar";
+  deleteBtn.classList.add("delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    taskItem.remove();
+    if (taskItem.classList.contains("completed")) {
+      completedTasksCount--;
+    }
+    tasksCount--; // Decrementa el contador de tareas totales
+    updateCounts();
+  });
+
+  taskButtons.appendChild(completeBtn);
+  taskButtons.appendChild(deleteBtn);
+
+  taskItem.appendChild(taskDescription);
+  taskItem.appendChild(taskButtons);
+
+  updateCounts();
+
+  return taskItem;
+}
+
